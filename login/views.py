1191936edee0df25898ae8bd54785d064100196e
Galymbekov <47265977+Galymbekov@users.noreply.github.com>
2021-04-29 15:12:49 +0600

    Progress

--- login/views.py
+++ login/views.py
@@ -8,17 +8,21 @@ from rest_framework.permissions import IsAuthenticated
 from django.http import JsonResponse
 
 
+class Auth(APIView):
+    permission_classes = (IsAuthenticated)
+
+
 class LoginUser(APIView):
-    def get(self, request):
-        serializer =LoginSerializer(data=request.data)
+    def list(self, request):
+        serializer = LoginSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.data, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
+
 @api_view(['GET'])
 def user_detail(request, user_id):
-
     try:
         user = Login.objects.get(id=user_id)
     except Login.DoesNotExist as e:
